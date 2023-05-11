e5e519af4361c39666974d07b1f9f4d2448446b4
OcimaOcram <96000963+OcimaOcram@users.noreply.github.com>
2023-05-11 20:02:20 +0200

    Cambiato logo, reso più responsive, più dinamico

--- descriptions.js
+++ descriptions.js
@@ -1,21 +1,3 @@
-// who pill
-let whoText = "My name is Marco Amico, and I am a Junior Web Developer. I recently completed my studies in Boolean, where I gained a solid foundation in web development. I have a passion for creating clean, functional code that enhances the user experience and is accessible to all.";
-
-const whoTextElement = document.getElementById("who-text");
-let currentIndex0 = 0;
-
-function addWhoText() {
-  if (currentIndex0 < whoText.length) {
-    whoTextElement.innerHTML += whoText.charAt(currentIndex0);
-    currentIndex0++;
-    setTimeout(addWhoText, 5);
-  }
-}
-
-
-document.getElementById("who-pill").addEventListener("mouseenter", () => {
-  addWhoText();
-});
 // training pill
 const trainingText = "During my course at Boolean, I gained a solid foundation of knowledge in web development, thanks to a structured and comprehensive educational program. I learned to master fundamental programming languages such as HTML, CSS, JavaScript, and PHP, developing responsive and dynamic websites using frameworks and CMSs such as Bootstrap and WordPress. I also acquired skills in database management with MySQL and code versioning with Git. Furthermore, thanks to collaboration with other students and practical project work, I learned to use agile methodologies such as SCRUM and developed problem-solving and critical analysis skills, which are fundamental for facing the challenges of the Junior Web Developer job market.";
 
@@ -32,6 +14,15 @@ function addTrainingText() {
 
 document.getElementById("training-pill").addEventListener("mouseenter", () => {
   addTrainingText();
+});
+
+const div1 = document.getElementById('training-text');
+const div2 = document.getElementById('training-pill');
+
+div2.addEventListener('mouseenter', () => {
+  div1.classList.remove('hide');
+  div2.classList.remove('rounded-pill');
+  div2.classList.add('rounded-top');
 });
 //experience pill
 const experienceText = "I had the opportunity to work on real projects and gain skills in different areas of programming, such as website development and database management. Additionally, I was able to collaborate with other interns and industry professionals, learning a lot from their work. The work environment was very stimulating and gave me the opportunity to apply the theoretical knowledge acquired during my studies.";
