0846ada05d85814c3f944524963a11761483378c
OcimaOcram <96000963+OcimaOcram@users.noreply.github.com>
2023-05-17 12:09:25 +0200

    Aggiunta form

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+// Verifica se il form è stato inviato
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Recupera i valori dal form
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  
+  // Imposta l'indirizzo email a cui inviare il messaggio
+  $to = "marco.amico.lavoro@gmail.com";
+  
+  // Imposta il soggetto dell'email
+  $subject = "Nuovo messaggio dal modulo di contatto";
+  
+  // Crea l'intestazione dell'email
+  $headers = "From: $name <$email>" . "\r\n";
+  
+  // Invia l'email
+  mail($to, $subject, $message, $headers);
+  
+  // Reindirizza l'utente a una pagina di conferma
+  header("Location: conferma.html");
+  exit();
+}
+?>
